2f295aec73235f4cd0a2b863b47619a8686156ac
Veraeze <Veraeze18@gmail.com>
2025-07-31 15:38:23 +0100

    Loaded dataset and saved preview for 2025-07-26_iris

--- scripts/09_publish_to_repo.py
+++ scripts/09_publish_to_repo.py
@@ -4,14 +4,11 @@ import subprocess
 
 # CONFIG
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-SOURCE_DATASETS_DIR = os.path.join(BASE_DIR, "..", "datasets")
-DEST_REPO_PATH = os.path.expanduser("~/ml_daily_models/ml_daily_models")  
-
-# Locate latest folder
-folders = sorted(os.listdir(SOURCE_DATASETS_DIR), reverse=True)
+datasets_dir = os.path.join(BASE_DIR, "..", "datasets")
+folders = sorted(os.listdir(datasets_dir), reverse=True)
 latest_folder = folders[0]
-source_path = os.path.join(SOURCE_DATASETS_DIR, latest_folder)
-dest_path = os.path.join(DEST_REPO_PATH, latest_folder)
+source_path = os.path.join(datasets_dir, latest_folder)
+dest_path = os.path.join(os.path.expanduser("~/ml_daily_models/ml_daily_models"), latest_folder)
 
 # Copy folder 
 if os.path.exists(dest_path):
@@ -22,7 +19,7 @@ shutil.copytree(source_path, dest_path)
 print(f"Copied {latest_folder} to ML-daily-models repo.")
 
 # Git add, commit, push ===
-os.chdir(DEST_REPO_PATH)
+os.chdir(os.path.expanduser("~/ml_daily_models/ml_daily_models"))
 subprocess.run(["git", "add", "."])
 subprocess.run(["git", "commit", "-m", f" Daily model update: {latest_folder}"])
 subprocess.run(["git", "push"])
