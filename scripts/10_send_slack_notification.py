5f853ca18194b3ec1ff86fb5e00a4d77e960aeb4
Veraeze <Veraeze18@gmail.com>
2025-08-01 04:09:37 +0100

    Loaded dataset and saved preview for 2025-07-26_iris

---
+++ scripts/10_send_slack_notification.py
@@ -0,0 +1,20 @@
+import requests
+import os
+
+# Load webhook URL
+SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
+
+# Read latest model folder
+datasets_dir = os.path.join(os.path.dirname(__file__), "..", "datasets")
+latest_run = sorted(os.listdir(datasets_dir), reverse=True)[0]
+
+message = {
+    "text": f" AutoML Agent has completed a new model run for *{latest_run}*. Check the latest results on GitHub!"
+}
+
+response = requests.post(SLACK_WEBHOOK_URL, json=message)
+
+if response.status_code != 200:
+    raise Exception(f"Slack notification failed: {response.status_code}, {response.text}")
+else:
+    print("Slack notification sent.")
